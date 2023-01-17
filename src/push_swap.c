6732403308e14b60db4389e3d39f8315a6cb0a79
fgeslin <fgeslin@student.42quebec.com>
2023-01-17 17:13:44 -0500

    add better_sort

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,13 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:43:24 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/01/05 16:01:07 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/01/17 16:04:37 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+int	sorted_at(t_list *lst);
 
 int	main(int argc, char const *argv[])
 {
@@ -22,7 +24,7 @@ int	main(int argc, char const *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error\n");
+		ft_putstr_fd("Error\n", 2);
 		return (-1);
 	}
 	stack_b = 0;
@@ -38,5 +40,6 @@ int	main(int argc, char const *argv[])
 		ft_lstadd_back(&stack_a, ft_lstnew((void *)n));
 	}
 	free(args);
+	printf("sorted? %d\n", sorted_at(stack_a));
 	sort(stack_a, stack_b);
 }
--- src/sorting.c
+++ src/sorting.c
@@ -6,11 +6,13 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 15:50:28 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/01/06 14:29:50 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/01/17 17:00:03 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+static void	better_sort(t_list **stack_a, t_list **stack_b);
 
 // static void	debugprint(t_list **a, t_list **b, char *f) // Debug
 // {
@@ -75,67 +77,148 @@ static void	choosefun(t_list **a, t_list **b, char *f)
 			}
 		}
 	}
+	better_sort(a, b);
 	// debugprint(a, b, f); // Debug
 }
 
-int	is_sorted(t_list *lst)
+// int	is_sorted(t_list *lst)
+// {
+// 	int	a;
+//
+// 	if (!lst)
+// 		return (0);
+// 	a = *(int *)lst->content;
+// 	while (lst->next)
+// 	{
+// 		if (a > *(int *)(lst->next->content))
+// 			return (0);
+// 		lst = lst->next;
+// 		a = *(int *)lst->content;
+// 	}
+// 	return (1);
+// }
+
+int	sorted_at(t_list *lst)
 {
-	int	a;
+	int		i;
+	int		ret;
+	int		a;
+	int		first;
+	t_list	*cur;
 
 	if (!lst)
 		return (0);
-	a = *(int *)lst->content;
-	while (lst->next)
+	cur = lst;
+	i = 0;
+	ret = 0;
+	a = *(int *)cur->content;
+	first = a;
+	while (cur->next)
 	{
-		if (a > *(int *)(lst->next->content))
-			return (0);
-		lst = lst->next;
-		a = *(int *)lst->content;
+		if (a > *(int *)(cur->next->content))
+		{
+			if (*(int *)cur->next->content > first || ret)
+				return (-1);
+			else if (*(int *)cur->next->content == first)
+				return (i);
+			first = *(int *)cur->next->content;
+			cur = cur->next;
+			a = *(int *)cur->content;
+			ret = i + 1;
+			// i = 0;
+			continue ;
+			// return (0);
+		}
+		cur = cur->next;
+		a = *(int *)cur->content;
+		i++;
 	}
-	return (1);
+	if (*(int *)cur->content > *(int *)lst->content && ret)
+		return (-1);
+	return (ret);
 }
 
-static void	sorting(t_list **stack_a, t_list **stack_b)
-{
-	t_list *temp;
+// static void	sorting(t_list **stack_a, t_list **stack_b)
+// {
+// 	t_list *temp;
+// 	int		sort;
+// 	int		size;
+//
+// 	size = ft_lstsize(*stack_a);
+// 	if (*stack_a)
+// 	{
+// 		sort = sorted_at(*stack_a);
+// 		if (sort > 0)
+// 		{
+// 			if (sort > size / 2)
+// 				choosefun(stack_a, stack_b, "rra");
+// 			else
+// 				choosefun(stack_a, stack_b, "ra");
+// 			return ;
+// 		}
+// 		if (!*stack_b && !sort)
+// 			return ;
+// 		choosefun(stack_a, stack_b, "sa");
+// 		return ;
+//
+// 		if ((*stack_a)->next)
+// 		{
+// 			if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
+// 			{
+// 				choosefun(stack_a, stack_b, "sa");
+// 				return ;
+// 			}
+// 		}
+// 		temp = *stack_b;
+// 		while(temp)
+// 		{
+// 			if (*(int *)(*stack_a)->content < *(int *)temp->content)
+// 			{
+// 				choosefun(stack_a, stack_b, "pa");
+// 				return ;
+// 			}
+// 			temp = temp->next;
+// 		}
+// 		if (is_sorted(*stack_a))
+// 			choosefun(stack_a, stack_b, "pa");
+// 		else
+// 			choosefun(stack_a, stack_b, "pb");
+// 		return ;
+// 	}
+// }
 
-	if (*stack_a)
-	{
-		if (!*stack_b && is_sorted(*stack_a))
-			return ;
-		if ((*stack_a)->next)
-		{
-			if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
-			{
-				choosefun(stack_a, stack_b, "sa");
-				sorting(stack_a, stack_b);
-				return ;
-			}
-		}
-		temp = *stack_b;
-		while(temp)
-		{
-			if (*(int *)(*stack_a)->content < *(int *)temp->content)
-			{
-				choosefun(stack_a, stack_b, "pa");
-				sorting(stack_a, stack_b);
-				return ;
-			}
-			temp = temp->next;
-		}
-		if (is_sorted(*stack_a))
-			choosefun(stack_a, stack_b, "pa");
-		else
+void	better_sort(t_list **stack_a, t_list **stack_b)
+{
+	int		sort;
+	int		size;
+
+	size = ft_lstsize(*stack_a);
+	if (size > 3)
 			choosefun(stack_a, stack_b, "pb");
-		sorting(stack_a, stack_b);
+	if (!*stack_a)
+		return ;
+	sort = sorted_at(*stack_a);
+	if (sort > 0)
+	{
+		if (sort > size / 2)
+			choosefun(stack_a, stack_b, "rra");
+		else
+			choosefun(stack_a, stack_b, "ra");
 		return ;
 	}
+	if (!sort)
+	{
+		if (!*stack_b)
+			return ;
+	}
+	choosefun(stack_a, stack_b, "sa");
+	return ;
 }
 
 void	sort(t_list *stack_a, t_list *stack_b)
 {
 	// debugprint(&stack_a, &stack_b, "init"); // Debug
-	sorting(&stack_a, &stack_b);
+	better_sort(&stack_a, &stack_b);
 	ft_lstclear(&stack_a, free);
 	ft_lstclear(&stack_b, free);
 }
