2ccc066affac73aa4f64f0abbdda6496822bf734
fgeslin <fgeslin@student.42quebec.com>
2023-01-18 15:53:55 -0500

    sort

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:43:24 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/01/17 16:04:37 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/01/18 13:47:03 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,16 @@ int	main(int argc, char const *argv[])
 	int		i;
 	int		*n;
 
-	if (argc != 2)
+	if (argc < 2)
 	{
 		ft_putstr_fd("Error\n", 2);
 		return (-1);
 	}
+	else if (argc == 2)
+		args = ft_split(argv[1], ' ');
+	else
+		args = (char **)argv + 1;
 	stack_b = 0;
-	args = ft_split(argv[1], ' ');
 	i = -1;
 	while (args[++i])
 	{
@@ -36,10 +39,12 @@ int	main(int argc, char const *argv[])
 		if (!n)
 			return (-1);
 		*n = ft_atoi(args[i]);
-		free(args[i]);
+		if (argc == 2)
+			free(args[i]);
 		ft_lstadd_back(&stack_a, ft_lstnew((void *)n));
 	}
-	free(args);
-	printf("sorted? %d\n", sorted_at(stack_a));
+	if (argc == 2)
+		free(args);
+	// printf("sorted? %d\n", sorted_at(stack_a));
 	sort(stack_a, stack_b);
 }
--- src/sorting.c
+++ src/sorting.c
@@ -6,35 +6,13 @@
 /*   By: fgeslin <fgeslin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 15:50:28 by fgeslin           #+#    #+#             */
-/*   Updated: 2023/01/17 17:13:55 by fgeslin          ###   ########.fr       */
+/*   Updated: 2023/01/18 15:29:07 by fgeslin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-static void	better_sort(t_list **stack_a, t_list **stack_b);
-
-// static void	debugprint(t_list **a, t_list **b, char *f) // Debug
-// {
-// 	printf("------\n");
-// 	printf("Exec '%s':\n", f);
-// 	while (*a || *b)
-// 	{
-// 		if (*a)
-// 		{
-// 			printf("%d", *(int *)(*a)->content);
-// 			a = &(*a)->next;
-// 		}
-// 		printf("\t");
-// 		if (*b)
-// 		{
-// 			printf("%d", *(int *)(*b)->content);
-// 			b = &(*b)->next;
-// 		}
-// 		printf("\n");
-// 	}
-// 	printf("-\t-\na\tb\n");
-// }
+// static void	better_sort(t_list **stack_a, t_list **stack_b);
 
 static void	choosefun(t_list **a, t_list **b, char *f)
 {
@@ -77,8 +55,7 @@ static void	choosefun(t_list **a, t_list **b, char *f)
 			}
 		}
 	}
-	better_sort(a, b);
-	// debugprint(a, b, f); // Debug
+	// better_sort(a, b);
 }
 
 // int	is_sorted(t_list *lst)
@@ -187,36 +164,99 @@ int	sorted_at(t_list *lst)
 // 	}
 // }
 
-void	better_sort(t_list **stack_a, t_list **stack_b)
-{
-	int		sort;
-	int		size;
+// void	better_sort(t_list **stack_a, t_list **stack_b)
+// {
+// 	int		sort;
+// 	int		size;
 
-	size = ft_lstsize(*stack_a);
-	if (size > 3)
-			choosefun(stack_a, stack_b, "pb");
-	if (!*stack_a)
-		return ;
-	sort = sorted_at(*stack_a);
-	if (sort > 0)
-	{
-		if (sort > size / 2)
-			choosefun(stack_a, stack_b, "rra");
-		else
-			choosefun(stack_a, stack_b, "ra");
-		return ;
-	}
-	if (sort == -1)
-		choosefun(stack_a, stack_b, "sa");
-	if (!*stack_b)
-		return ;
+// 	size = ft_lstsize(*stack_a);
+// 	if (size > 3)
+// 			choosefun(stack_a, stack_b, "pb");
+// 	if (!*stack_a)
+// 		return ;
+// 	sort = sorted_at(*stack_a);
+// 	if (sort > 0)
+// 	{
+// 		if (sort > size / 2)
+// 			choosefun(stack_a, stack_b, "rra");
+// 		else
+// 			choosefun(stack_a, stack_b, "ra");
+// 		return ;
+// 	}
+// 	if (sort == -1)
+// 		choosefun(stack_a, stack_b, "sa");
+// 	if (!*stack_b)
+// 		return ;
 		
+// }
+
+int	put_after(t_list *lst, int a)
+{
+	int	min;
+	int	max;
+
+	min = *(int *)lst->content;
+	max = *(int *)lst->content;
+	while (lst->next)
+	{
+		if (*(int *)lst->content < min)
+			min = *(int *)lst->content;
+		if (*(int *)lst->next->content > max)
+			max = *(int *)lst->next->content;
+		// printf ("HUH\n");
+		if (*(int *)lst->content < a && *(int *)lst->next->content > a)
+// {			printf("%d\n", *(int *)lst->content);
+		return (*(int *)lst->content);
+// }
+		lst = lst->next;
+	}
+	return (max);
+}
+
+int	get_index(t_list *lst, int a)
+{
+	int i = 0;
+
+	while (*(int *)lst->content != a)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
 }
 
 void	sort(t_list *stack_a, t_list *stack_b)
 {
-	// debugprint(&stack_a, &stack_b, "init"); // Debug
-	better_sort(&stack_a, &stack_b);
+	int	ind;
+	// better_sort(&stack_a, &stack_b);
+
+	while (sorted_at(stack_a) == -1)
+	{
+		if (ft_lstsize(stack_a) > 3)
+			choosefun(&stack_a, &stack_b, "pb");
+		else
+			choosefun(&stack_a, &stack_b, "sa");
+	}
+	while (stack_b)
+	{
+		ind = get_index(stack_a, put_after(stack_a, *(int *)stack_b->content));
+		// printf ("HUH %d\n", ind);
+		if (ind + 1 > ft_lstsize(stack_a) / 2)
+			while (++ind < ft_lstsize(stack_a))
+				choosefun(&stack_a, &stack_b, "rra");
+		else
+			while (--ind >= 0)
+				choosefun(&stack_a, &stack_b, "ra");
+		choosefun(&stack_a, &stack_b, "pa");
+	}
+	while (sorted_at(stack_a) > 0)
+	{
+		if (sorted_at(stack_a) > ft_lstsize(stack_a) / 2)
+			choosefun(&stack_a, &stack_b, "rra");
+		else
+			choosefun(&stack_a, &stack_b, "ra");
+	}
+
 	ft_lstclear(&stack_a, free);
 	ft_lstclear(&stack_b, free);
 }
